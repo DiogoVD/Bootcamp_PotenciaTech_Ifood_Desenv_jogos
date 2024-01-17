77815cce3f3ac5c7d6c1951d9aeeefbea7f6b91b
DiogoVD <diogovdamascena@gmail.com>
2024-01-17 17:49:19 -0300

    finalizado protorype

--- Estudo_JS/orientacao_prototipo.js
+++ Estudo_JS/orientacao_prototipo.js
@@ -12,3 +12,41 @@ const diogo = {
 }
 
 console.log(diogo.genero)
+
+//funçoes construtoras
+
+function Pessoa(nome, idade){
+    this.nome = nome
+    this.idade = idade
+}
+
+Pessoa.prototype.falar = function(){
+    console.log(`Meu nome é: ${this.nome}`)
+}
+
+const diana = new Pessoa('Diana', 10)
+
+diana.falar()
+
+// o codigo abaixo é equivalente ao codigo acima
+/*
+class Pessoa{
+    constructor(nome, idade){
+        this.nome = nome
+        this.idade = idade
+    }
+    falar(){
+        console.log(`Meu nome é: ${this.nome}`)
+    }
+}
+*/
+//----------------------------------------------
+
+// Criando propriedades de objetos usando prototype
+
+String.prototype.toPlang = function() {
+    return `P ${this}`
+}
+//Plang é uma propriedade que esta sendo criada.
+
+console.log('teste'.toPlang())
