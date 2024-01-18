feedfa26452757c1c246829c99a0d653ef743ead
DiogoVD <diogovdamascena@gmail.com>
2024-01-18 08:48:03 -0300

    estudando promisses com arquivos

--- Estudo_JS/promisses.js
+++ Estudo_JS/promisses.js
@@ -19,4 +19,70 @@ promessaDeUmNumero
         console.log('Finalizou')
     })
 
+const { error } = require('console')
     //------------------------------------------------------------------
+
+    //Leitura de arquivos
+
+    const fs = require('fs')
+
+    const promessaArquivo = fs.promises.readFile('tarefas.csv')
+
+    promessaArquivo
+        .then((arquivo) => {
+            console.log(arquivo.toString('utf-8'))
+        })
+        .catch((error) => {
+            console.log('Leitura falhou')
+        })
+        .finally(() => {
+            console.log('---------------------------------------------')
+        })
+
+// outro metodo para chamada de arquivo definindo o caminho
+
+const fs2 = require('fs')
+const path = require('path')
+
+const caminhoArquivo = path.resolve(__dirname, 'tarefas.csv')
+
+const promessaArquivo2 = fs2.promises.readFile(caminhoArquivo)
+
+promessaArquivo2
+    .then((arquivo) => {
+        console.log(arquivo.toString('utf-8'))
+    })
+    .catch((error) => {
+        console.log('Leitura falhou', error)
+    })
+    .finally(() => {
+        console.log('---------------------------------------------')
+    })
+
+//--------------------------------------------------------
+//Brincando com promisses
+
+const fs3 = require('fs')
+const path3 = require('path')
+
+const caminhoArquivo3 = path3.resolve(__dirname, 'tarefas.csv')
+
+const promessaArquivo3 = fs2.promises.readFile(caminhoArquivo3)
+
+promessaArquivo3
+    .then((arquivo3) => arquivo3.toString('utf-8'))                     //converte para string utf8
+    .then((textoArquivo) => textoArquivo.split('\n').slice(1))          // pega o texto convertido em string e separa por linha e elimina o cabeçalho.
+    .then((linhasSemCabecalho) => linhasSemCabecalho.map((linha) => {   // Realiza a conversão de cada linha em um array de chave valor.
+        const [nome, feito] = linha.split(';')                   
+            return {
+                nome,
+                feito: feito.trim() === 'true'  // o trim remove os espaços vazios da string
+            }
+    }))
+    .then((listaTarefas) => console.log(listaTarefas))
+    .catch((error3) => {
+        console.log('Leitura falhou', error3)
+    })
+    .finally(() => {
+        console.log('---------------------------------------------')
+    })
