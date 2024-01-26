921da3ac4bdbaaa08f994e04d1cfa2674280a19b
DiogoVD <diogovdamascena@gmail.com>
2024-01-25 21:18:25 -0300

    detona ralph finalizado

--- Desafios/Detona_Ralph/src/scripts/engine.js
+++ Desafios/Detona_Ralph/src/scripts/engine.js
@@ -9,8 +9,13 @@ const state = {
     /*Altera a parte de codificação*/
     values:{
         timerID: null,
-        gameVelocity: 1000,
-
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+    },
+    actions:{
+        timerID: setInterval(randomSquare, 1000), //velocidade do jogo em 1000 para aumentar a dificuldade esse tempo deve diminuir.
+        countDownTimerId: setInterval(countDown, 1000),
     }
 }
 
@@ -22,20 +27,43 @@ function randomSquare(){
     let randomNumber = Math.floor(Math.random() * 9);
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
 }
 
-function moveEnemy(){
-    state.values.timerID = setInterval(randomSquare, state.values.gameVelocity);
+function countDown(){
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <= 0){
+        clearInterval(state.actions.countDownTimerId)
+        clearInterval(state.actions.timerID)
+        alert(`GAME OVER! O seu resultado foi: ${state.values.result};`)
+    }
 }
+
+function playsound(audioName){
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+}
+
 
 function addListenerHitBox(){
     state.view.squares.forEach((square) => {
-
+        square.addEventListener("mousedown", () => {
+            if (square.id === state.values.hitPosition){
+                state.values.result++
+                state.view.score.textContent = state.values.result
+                state.values.hitPosition = null
+                playsound("hit")
+            }
+        });
     });
 }
 
+
+
 function init(){
-    moveEnemy();
     addListenerHitBox();
 }
 
