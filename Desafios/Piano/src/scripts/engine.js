cd4ea2da6b4a302c004468061802ac97d01c27ed
DiogoVD <diogovdamascena@gmail.com>
2024-01-29 23:24:09 -0300

    Adicionando audio ao projeto

---
+++ Desafios/Piano/src/scripts/engine.js
@@ -0,0 +1,14 @@
+
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+
+let audio = new Audio();
+
+const playTune = (key) => {
+    audio.src = `./src/tunes/${key}.wav`;
+    audio.play();
+};
+
+pianoKeys.forEach((key) => {
+    console.log(key.dataset.tecla);
+    key.addEventListener("click", () => playTune(key.dataset.tecla));
+});
