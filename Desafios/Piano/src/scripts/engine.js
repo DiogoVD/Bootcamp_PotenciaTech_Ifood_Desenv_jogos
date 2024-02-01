82c88be3c3cd68a987e6458490dfd0b6f0ecc7b5
DiogoVD <diogovdamascena@gmail.com>
2024-02-01 10:18:45 -0300

    continuand

--- Desafios/Piano/src/scripts/engine.js
+++ Desafios/Piano/src/scripts/engine.js
@@ -1,18 +1,29 @@
 
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let mappedKeys = [];
 let audio = new Audio();
 
 const playTune = (key) => {
     audio.src = `./src/tunes/${key}.wav`;
     audio.play();
+
+    const clickedKey =document.querySelector(`[data-tecla="${key}"]`);
+    clickedKey.classList.add("active");
+
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
+
 };
 
 pianoKeys.forEach((key) => {
-    console.log(key.dataset.tecla);
     key.addEventListener("click", () => playTune(key.dataset.tecla));
+    mappedKeys.push(key.dataset.tecla);
 });
 
-document.addEventListener("keydown", (teclaCaptura) => {
-    playTune(teclaCaptura.key);
+document.addEventListener("keydown", (e) => {
+    if (mappedKeys.includes(e.key)){
+        playTune(e.key);
+    }
 });
